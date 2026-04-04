faef98aafc8f2d2e2bacbfccde13f023f427e9ca
Mohnish <mohnish.a007@gmail.com>
2026-04-04 01:06:14 +0100

    Strings in C

--- memmanagement/playstring.c
+++ memmanagement/playstring.c
@@ -3,6 +3,8 @@ int main() {
     printf("Charater and Strings\n");
     char c = 'a';
     printf(" the char is %c \n",c);
-    
+    char *s = "abc";
+    //char *s = 'abc'; // This will make it a intiger not string pay attention to " " and ' '. 
+    printf(" the string is %s \n",s);
 return 0;
 }
