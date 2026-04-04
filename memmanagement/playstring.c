8d4c1a3682c03549691e54b27aa9f698ee2d0acb
Mohnish <mohnish.a007@gmail.com>
2026-04-04 01:09:13 +0100

    Perfect char and strings in C

--- memmanagement/playstring.c
+++ memmanagement/playstring.c
@@ -5,8 +5,8 @@ int main() {
     printf(" the char is %c \n",c);
     char *s = "abc";
     //char *s = 'abc'; // This will make it a intiger not string pay attention to " " and ' '. 
-    //printf(" the string is %s \n",s);
-    printf(" the string is %c \n",*s);
+    printf(" the string is %s \n",s);
+    //printf(" the string is %c \n",*s);
     //printf(" the string is %s \n",*s); This will create problem as *s can only point to first location of the string
 return 0;
 }
