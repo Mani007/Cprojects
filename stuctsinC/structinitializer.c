d3fb4b845c5cda4db652c529b3542be4f6323123
Mohnish <mohnish.a007@gmail.com>
2026-04-06 23:55:54 +0100

    Fully working struct in C with positional initializer

--- stuctsinC/structinitializer.c
+++ stuctsinC/structinitializer.c
@@ -8,5 +8,9 @@ int main() {
     City c1 = {0}; // initialize everything in the struct as 0 or null
     printf("City id is %d\n",c1.id);
     printf("City id is %s\n",c1.name);
+    // Positional initializer
+    City c2 = {1,"ABC"};
+    printf("City id is %d\n",c2.id);
+    printf("City id is %s\n",c2.name);
 return 0;
 }
