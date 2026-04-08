db2daca7115fac743c2119033cb93aa6be35b56e
Mohnish <mohnish.a007@gmail.com>
2026-04-08 23:02:51 +0100

    Pointer to struct and arrow operator

---
+++ Pointer/arrowoperator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+typedef struct {
+    int x;
+    int y;
+} Point;
+int main() {
+    printf("Arrow operator to Pointer \n");
+    Point p = {
+        .x = 10,
+        .y = 15
+    };
+    Point *ptr = &p;
+    printf("The value of x using simple method is %d \n", p.x);
+    printf("The value of x using pointer casting is %d \n", (*ptr).x);
+    printf("The value of x using pointer arrow operator is %d \n", ptr->x);
+return 0;
+}
