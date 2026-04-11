71f69597dcaf2c51e3a7cfe7890a598c39b289f7
Mohnish <mohnish.a007@gmail.com>
2026-04-11 01:34:44 +0100

    Pointer to array with some error

--- Pointer/arraypointer.c
+++ Pointer/arraypointer.c
@@ -2,8 +2,8 @@
 int main() {
     printf("Array and pointer \n");
     int arr[5] = {100,200,300,400,500};  // declaring int array with some initial values
-    int *ptr = &arr;
-    int *ptr = &arr;
+    //int *ptr = &arr;
+    int (*ptr)[5] = &arr;
     printf("Value at index 0 is %d \n", *ptr);
     printf("Value at index 1 is %d \n", *(ptr+1));
 return 0;
