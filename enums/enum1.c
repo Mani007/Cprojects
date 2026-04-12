3fb89157f91803a4f08ee24a7d042b00a2af246c
Mohnish <mohnish.a007@gmail.com>
2026-04-12 21:46:54 +0100

    Simple program of enum with switch cases

--- enums/enum1.c
+++ enums/enum1.c
@@ -9,17 +9,39 @@ typedef enum alphabets5 {
     E  //is 4
 } A5;
 
-typedef struct grade{
-    A5 quality;
-    int size;
-} Grade;
+// typedef struct grade{
+//     A5 quality;
+//     int size;
+// } Grade;
 int main() {
     printf("Enums in C \n");
-    Grade* g = (Grade*)malloc(sizeof(Grade));
-    g->quality = C;
-    g->size = 20;
-    printf("The value of quality is %d\n", g->quality);
-    free(g); // always free the memory allocated
+    A5 grade = B;
+    // Grade* g = (Grade*)malloc(sizeof(Grade));
+    // g->quality = C;
+    // g->size = 20;
+    // printf("The value of quality is %d\n", g->quality);
+    // free(g); // always free the memory allocated
+    switch (grade)
+    {
+    case A:
+        printf("Enum case of A is %d \n", A);
+        break;
+    case B:
+        printf("Enum case of B is %d \n", B);
+        break;
+    case C:
+        printf("Enum case of C is %d \n", C);
+        break;
+    case D:
+        printf("Enum case of D is %d \n", D);
+        break;
+    case E:
+        printf("Enum case of E is %d \n", E);
+        break;
+    
+    default:
+        break;
+    }
 
 return 0;
 }
