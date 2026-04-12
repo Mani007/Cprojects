c3a4309f0f1ad301378fb6bfe32c2e39c2771319
Mohnish <mohnish.a007@gmail.com>
2026-04-12 21:36:58 +0100

    Simple program with nun default value of enum with auto increments in C

--- enums/enum1.c
+++ enums/enum1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 typedef enum alphabets5 {
-    // Adding some non default value to enum
+    // Adding some non default value to enum and auto increment
     A =95, // is 0
-    B = 85, // is 1
-    C = 50, // is 2
-    D = 15, // is 3
-    E = 10 //is 4
+    B , // is 1
+    C , // is 2
+    D , // is 3
+    E  //is 4
 } A5;
 int main() {
     printf("Enums in C \n");
