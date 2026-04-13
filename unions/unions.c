f8d276d11f28200bcae1f157bf34a3d829d8e791
Mohnish <mohnish.a007@gmail.com>
2026-04-13 23:04:33 +0100

    Simple program for union in and its size

---
+++ unions/unions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef union uni  // Union always allocate memory of the largest size of datatype, here its int
+{
+    int id;
+    char s;
+} Uni;  // This memory is allocated in continuous memory layout
+
+int main() {
+    printf("Unions in C\n");
+    //Uni u;
+    Uni *u1 = (Uni *) malloc(sizeof(Uni)); // allocating memory for char
+    Uni *u2 = (Uni *) malloc(sizeof(Uni)); // allocating memory for int
+    u1->s = "A";
+    u2->id = 30;
+    printf("The size of Uni U1 with string is %d\n",sizeof(u1)); 
+    printf("The size of Uni U2 with int is %d\n",sizeof(u2)); 
+return 0;
+}
